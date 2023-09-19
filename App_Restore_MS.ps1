3198942d6032723215c47145097e3a4fb86fe59d
Oppilaal Daniel <doppilaal11@gmail.com>
2023-09-19 20:20:45 +0530

    Add files via upload
    This PowerShell script helps in the restoration of Applications only to Managed Space. It takes all the necessary details from the Development copy of the Application in Shared Space combined with the updated data and Community Sheets from the Backup folder.

---
+++ App_Restore_MS.ps1
@@ -0,0 +1,34 @@
+﻿#################################################
+# Configurable Variables #
+#################################################
+# Specify the target shared space for restoration, e.g. 1846a41c-edf6-474d-ad79-d20c1c13cb57
+$sharedSpaceAppId = 'ec80858f-4b87-49da-a7b6-bb94d8ae8319'    #AppID of the Application in Shared space
+# Specify the target managed space for publish, e.g. 5e42f31c5d851d0001f8ca56
+$managedSpaceId = '63109deacdc002266e7e013e'     #Managed Space ID
+# Specify the location for the backed up app, e.g. C:\tmp\admin-playbook-unbuild-2022-02-22
+$restoreDirectory = 'C:\Users\Oppilaale\Documents\Projects\Internal Activities\Backups\tips-and-tricks-unbuild-2022-09-01'    #App Backup folder path
+
+#################################################
+# Main Logic #
+#################################################
+# GOTO the restored app's directory
+Set-Location $restoreDirectory
+
+# Publish the app to a managed space
+$publishedApp = qlik app publish create $sharedSpaceAppId --data source --spaceId $managedSpaceId | ConvertFrom-Json
+
+# GOTO the Community sheets directory & Publish them on the app version in the managed space
+Set-Location '.\sheets-community'
+$communitySheetObjects = Get-ChildItem -Filter sheet*.json
+$communitySheetObjects | %{
+ qlik app object set $_ --app $($publishedApp.attributes.id)
+ $sheetInfo = gc $_ | ConvertFrom-Json
+ qlik app object publish $($sheetInfo.qProperty.qInfo.qId) --app $($publishedApp.attributes.id)
+}
+
+# GOTO the Community sheets directory & Publish them on the app version in the managed space
+Set-Location '..\sheets-private'
+$communitySheetObjects = Get-ChildItem -Filter sheet*.json
+$communitySheetObjects | %{
+ qlik app object set $_ --app $($publishedApp.attributes.id)
+}
